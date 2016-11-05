d79c472934ac3a6ed4b3d5c1d6f7baed70b3151f
Tyler Ches <ttches@gmail.com>
2016-11-05 13:53:56 -0700

    Concatenated JavaScript Files

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,21 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
+var jsSources = ['components/scripts/*.js'];
+
 gulp.task('coffee', function() {
   gulp.src(coffeeSources)
     .pipe(coffee({bare: true})
       .on('error', gutil.log))
     .pipe(gulp.dest('components/scripts'));
+});
+
+gulp.task('js', function() {
+  gulp.src(jsSources)
+    .pipe(concat('script.js')
+      .on('error', gutil.log))
+    .pipe(gulp.dest('builds/development/js'));
 });
