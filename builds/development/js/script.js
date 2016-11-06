d6db2fe28c34a4438f2318a757c85528d3b6c329
Tyler Ches <ttches@gmail.com>
2016-11-06 11:05:10 -0700

    Added gulp watch

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -79,7 +79,7 @@ $ = require('jquery');
 
 (fill = function(item) {
   return $('.tagline').append("" + item);
-})('The most creative minds in Art');
+})('The most creative minds in Art.');
 
 fill;
 
