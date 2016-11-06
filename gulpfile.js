0398456925c98e481645b8303f1680efffe005e4
Tyler Ches <ttches@gmail.com>
2016-11-06 16:38:28 -0700

    Added environment variable

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,31 @@ var gulp = require('gulp'),
     compass = require('gulp-compass'),
     connect = require('gulp-connect');
 
-var coffeeSources = ['components/coffee/tagline.coffee'];
-var jsSources = ['components/scripts/*.js'];
-var sassSources = ['components/sass/style.scss'];
-var htmlSources = ['builds/development/*.html'];
+var env,
+  coffeeSources,
+  jsSources,
+  sassSources,
+  htmlSources,
+  jsonSources,
+  outputDir,
+  sassStyle;
+
+var env = process.env.NODE_ENV || 'development';
+
+
+if (env==='development') {
+  outputDir = 'builds/development/';
+  sassStyle = 'expanded';
+} else {
+  outputDir = 'builds/production/';
+  sassStyle = 'compressed';
+}
+
+coffeeSources = ['components/coffee/tagline.coffee'];
+jsSources = ['components/scripts/*.js'];
+sassSources = ['components/sass/style.scss'];
+htmlSources = [outputDir +'*.html'];
+jsonSources = [outputDir + 'js/*.json'];
 
 gulp.task('coffee', function() {
   gulp.src(coffeeSources)
@@ -23,7 +44,7 @@ gulp.task('js', function() {
     .pipe(concat('script.js')
       .on('error', gutil.log))
     .pipe(browserify())
-    .pipe(gulp.dest('builds/development/js'))
+    .pipe(gulp.dest(outputDir + 'js'))
     .pipe(connect.reload());
 });
 
@@ -31,11 +52,12 @@ gulp.task('compass', function() {
   gulp.src(sassSources)
     .pipe(compass({
       sass: 'components/sass',
-      image: 'builds/development/images',
-      style: 'expanded'
+      image: outputDir + 'images',
+      style: sassStyle,
+      comments: true
     })
       .on('error', gutil.log))
-    .pipe(gulp.dest('builds/development/css'))
+    .pipe(gulp.dest(outputDir + 'css'))
     .pipe(connect.reload());
 });
 
@@ -43,13 +65,13 @@ gulp.task('watch', function() {
   gulp.watch(coffeeSources, ['coffee']);
   gulp.watch(jsSources, ['js']);
   gulp.watch(htmlSources, ['html']);
-  gulp.watch('builds/development/js/*.json', ['json']);
+  gulp.watch(jsonSources, ['json']);
   gulp.watch('components/sass/*.scss', ['compass']);
 });
 
 gulp.task('connect', function() {
   connect.server({
-    root: 'builds/development/',
+    root: outputDir,
     livereload: true
   });
 });
@@ -60,7 +82,7 @@ gulp.task('html', function() {
 });
 
 gulp.task('json', function() {
-  gulp.src('builds/development/js/*.json')
+  gulp.src(jsonSources)
   .pipe(connect.reload());
 });
 
