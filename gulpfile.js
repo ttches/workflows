7f84c1824ef2ccfb7e19185ac261e7f34cf291db
Tyler Ches <ttches@gmail.com>
2016-11-06 17:01:37 -0700

    Added minify HTML

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,8 @@ var gulp = require('gulp'),
     compass = require('gulp-compass'),
     connect = require('gulp-connect'),
     gulpif = require('gulp-if'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    minifyHTML = require('gulp-minify-html');
 
 var env,
   coffeeSources,
@@ -67,7 +68,7 @@ gulp.task('compass', function() {
 gulp.task('watch', function() {
   gulp.watch(coffeeSources, ['coffee']);
   gulp.watch(jsSources, ['js']);
-  gulp.watch(htmlSources, ['html']);
+  gulp.watch('builds/development/*html', ['html']);
   gulp.watch(jsonSources, ['json']);
   gulp.watch('components/sass/*.scss', ['compass']);
 });
@@ -80,7 +81,9 @@ gulp.task('connect', function() {
 });
 
 gulp.task('html', function() {
-  gulp.src(htmlSources)
+  gulp.src('builds/development/*html')
+  .pipe(gulpif(env === 'production', minifyHTML()))
+  .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
   .pipe(connect.reload());
 });
 
